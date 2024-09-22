c2b06e479a1b626a9ccb2edaa56e201e38869e16
George Ibarra <gibarr13@stu.southtexascollege.edu>
2024-09-21 21:46:28 -0500

    Resubmit

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,80 @@
 <script setup>
   import Header from './components/Header.vue';
   import Balance from './components/Balance.vue';
-  import IncomeExpenses from './components/IncomeExpenses.vue';
+  import IncomeExpense from './components/IncomeExpense.vue';
   import AddTransaction from './components/AddTransaction.vue';
-  import TransactionList from './components/TransactionList.vue';l
-
+  import TransactionList from './components/TransactionList.vue';
   import {ref, computed, onMounted} from 'vue'
 
+  //array 
   const transactions = ref([])
 
+  //x represents one element in the transaction
+  //sum is a variable used to display the total sum of the array
   const sum = computed(()=>{
-    return transactions.value.reduce((acc, x)=>{
+    return transactions.value.reduce((acc, x) =>{
+      //running total 
       return acc+x.amount
-    },0)
+    },0) 
   })
-   
-    const moneyIn = computed(()=>{
+
+  const moneyIn = computed(()=>{
     return transactions.value
     .filter((x)=>x.amount>0)
-    .reduce((acc, x)=>{
+    .reduce((acc, x) =>{
+      //running total 
       return acc+x.amount
-    },0)
- })
+    },0) 
+  })
 
- const moneyOut = computed(()=>{
+  const moneyOut = computed(()=>{
     return transactions.value
     .filter((x)=>x.amount<0)
-    .reduce((acc, x)=>{
+    .reduce((acc, x) =>{
+      //running total 
       return acc+x.amount
-    },0)
- })
-
- const handleTransaction = (transactionData) => {
+    },0) 
+  })
+  
+  const handleTransaction = (transactionData) => {
     transactions.value.push({
       text: transactionData.text,
       amount: transactionData.amount,
-    })  
+    })
     saveToLocalStorage()
- }
   }
 
+  const generateID = () =>{
+    return Math.floor(Math.random()*10000000)
+  }
+
+  //setting up the x to delete the lists of expenses
   const handleDelete = (id) => {
     transactions.value = transactions.value.filter((x) => x.id !== id)
     saveToLocalStorage()
   }
 
-const saveToLocalStorage = () => {
-  localStorage.setItem('transaction', JSON.stringify(transactions.value))
-}
+  const saveToLocalStorage = () =>{
+    localStorage.setItem('transactions', JSON.stringify(transactions.value))
+  }
 
-  onMounted( () => {
-    const savedTransactions = JSON.parse(localStorage.getItem('transaction'))
+  onMounted(() => {
+    const savedTransactions = JSON.parse(localStorage.getItem('transactions'))
 
     if(savedTransactions){
       transactions.value = savedTransactions
     }
-  }) 
+  })
+
+
+ 
 </script>
 
 <template>
   <Header></Header>
   <div class="container">
-    <Balance :total ="sum"></Balance>
-    <IncomeExpenses :income="moneyIn" :expense="moneyOut"></IncomeExpenses>
+    <Balance :total="sum"></Balance>
+    <IncomeExpense :income="moneyIn" :expense="moneyOut"></IncomeExpense>
     <AddTransaction @transactionSubmitted="handleTransaction"></AddTransaction>
     <TransactionList :transactions="transactions" @transactionDeleted="handleDelete"></TransactionList>
   </div>
